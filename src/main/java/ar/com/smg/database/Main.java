3b95acb4e96642788df2be3146c5ff7dbfb22244
JuLima14 <julian-lima@live.com>
2017-02-03 12:06:48 -0300

    commit

--- src/main/java/ar/com/smg/database/Main.java
+++ src/main/java/ar/com/smg/database/Main.java
@@ -2,17 +2,19 @@ package ar.com.smg.database;
 
 import java.sql.SQLException;
 
+import org.hibernate.Session;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String output = "Jersey say : nada";
+		
 		try {
-			new DbContext().getConnection() ;
-			System.out.println("llegamo papa");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		Session session = HibernateUtil.getSessionFactory().openSession();
+			session.beginTransaction();
+			
+			session.createNamedQuery("select * from pantalla").executeUpdate();
+			session.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ src/main/java/ar/com/smg/pantalla/beans/Pantalla.java
@@ -0,0 +1,39 @@
+package ar.com.smg.pantalla.beans;
+
+public class Pantalla {
+	
+	
+	private int id;
+	private String nombre;
+	private String url;
+	
+	public Pantalla(int id , String nombre , String url){
+		this.id = id;
+		this.nombre = nombre;
+		this.url = url;
+	}
+	public Pantalla(){
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+
+}
